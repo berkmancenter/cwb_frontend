7d688a54b810ae999b373842560965d5a375b19a
Ryan Clardy <ryan@metabahn.com>
2014-09-13 01:22:40 -0500

    Clean out commented code

--- apps/cwb/resources/queries.js
+++ apps/cwb/resources/queries.js
@@ -15,15 +15,3 @@ CWB.SELECTED_FILES_QUERY =
    SC.Query.local(CWB.File, 'isSelected = YES');
 
 CWB.SELECTED_FILES = null;
-
-// CWB.TERMS_IN_VOCABULARY_QUERIES = [
-//     null,
-//     SC.Query.local(CWB.Term, {conditions:'vocabulary.id = {id}', parameters: {id: 1}}),
-//     SC.Query.local(CWB.Term, {conditions:'vocabulary.id = {id}', parameters: {id: 2}}),
-//     SC.Query.local(CWB.Term, {conditions:'vocabulary.id = {id}', parameters: {id: 3}}),
-//     SC.Query.local(CWB.Term, {conditions:'vocabulary.id = {id}', parameters: {id: 4}}),
-//     SC.Query.local(CWB.Term, {conditions:'vocabulary.id = {id}', parameters: {id: 5}}),
-//     SC.Query.local(CWB.Term, {conditions:'vocabulary.id = {id}', parameters: {id: 6}})
-// ];
-
-// CWB.TERMS_IN_VOCABULARY = [null, null, null, null, null, null];
