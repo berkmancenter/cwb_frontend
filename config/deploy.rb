2685ad10bb4832e16ab9902038015df789359c4d
Ryan Clardy <ryan@metabahn.com>
2014-11-16 20:07:48 -0600

    Update project names in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,15 @@
 # config valid only for Capistrano 3.1
 lock '3.2.1'
 
-set :application, 'cwb-sc'
-set :repo_url, 'git@git.metabahn.net:client/cwb-sc.git'
+set :application, 'fwb_frontend'
+set :repo_url, 'git@git.metabahn.net:client/fwb_frontend.git'
 # set :branch, :develop
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
 # Default deploy_to directory is /var/www/my_app
-set :deploy_to, '/var/apps/cwb-sc'
+set :deploy_to, '/var/apps/fwb_frontend'
 
 # Default value for :scm is :git
 set :scm, :git
@@ -20,7 +20,7 @@ namespace :deploy do
   desc 'Create sc release'
   task :sc_release do
     on roles(:app) do
-      execute "cd /var/apps/cwb-sc/current; /root/.rbenv/shims/bundle exec sproutcore build cwb --buildroot /var/apps/cwb/shared/public"
+      execute "cd /var/apps/fwb_frontend/current; /root/.rbenv/shims/bundle exec sproutcore build cwb --buildroot /var/apps/fwb/shared/public"
     end
   end
 
